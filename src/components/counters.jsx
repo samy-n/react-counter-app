a775fa829c5d152ba778058c1a650b0c01b13525
Samyukta Nair <samycode00@gmail.com>
2020-04-03 16:37:31 +0530

    Adding all files

---
+++ src/components/counters.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    const { onDelete, onReset, onIncrement, counters } = this.props;
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary">
+          Reset
+        </button>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
